0bc74350c31f5559a7cc3e74064bc7d56cb329e2
Jeffrey Cheng <imjeffrey23@gmail.com>
2020-12-14 22:45:35 -0500

    script

--- blink-test.py
+++ blink-test.py
@@ -5,14 +5,19 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 ledPin = 26
+led2Pin = 19
 GPIO.setup(ledPin, GPIO.OUT)
 
 try:
 	while(True):
 		GPIO.output(ledPin, GPIO.HIGH)
-		time.sleep(3)
-		GPIO.output(ledPin, GPIO.LOW) 
-		time.sleep(1)
+		time.sleep(1.5)
+		GPIO.output(led2Pin,GPIO.HIGH)
+		time.sleep(1.5)
+		GPIO.output(led2Pin, GPIO.LOW) 
+		time.sleep(0.5)
+		GPIO.output(ledPin,GPIO.LOW)
+		time.sleep(0.5
 
 # End program cleanly with keyboard
 except KeyboardInterrupt:
