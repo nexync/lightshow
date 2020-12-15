826b5d6370bfd80ab2eabe212728297ab49ad9d9
Jeffrey Cheng <imjeffrey23@gmail.com>
2020-12-14 22:09:42 -0500

    push

--- blink-test.py
+++ blink-test.py
@@ -4,17 +4,13 @@ import time
 #GPIO.setmode(GPIO.BOARD)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-ledPin = 13
+ledPin = 26
 GPIO.setup(ledPin, GPIO.OUT)
 
 try:
 	while(True):
 		GPIO.output(ledPin, GPIO.HIGH)
-		time.sleep(0.25)
-		GPIO.output(ledPin, GPIO.LOW) 
-		time.sleep(0.25)
-		GPIO.output(ledPin, GPIO.HIGH)
-		time.sleep(0.1)
+		time.sleep(3)
 		GPIO.output(ledPin, GPIO.LOW) 
 		time.sleep(1)
 
