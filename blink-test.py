5f3cfa912b1e279a29c4611951af9b32de3b95ba
Jeffrey Cheng <imjeffrey23@gmail.com>
2020-12-14 20:01:48 -0500

    blink

--- blink-test.py
+++ blink-test.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+#GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 ledPin = 13
 GPIO.setup(ledPin, GPIO.OUT)
@@ -13,3 +14,5 @@ for i in range(5):
 	print("LED turning off.")
 	GPIO.output(ledPin, GPIO.LOW) 
 	time.sleep(0.5)
+
+GPIO.cleanup()
