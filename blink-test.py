899761917b03a4dd64b228958d380e6a6ce5370a
Jeffrey Cheng <imjeffrey23@gmail.com>
2020-12-14 22:49:18 -0500

    bug

--- blink-test.py
+++ blink-test.py
@@ -13,12 +13,13 @@ try:
 	while(True):
 		GPIO.output(ledPin, GPIO.HIGH)
 		time.sleep(1.5)
+		GPIO.output(ledPin,GPIO.LOW)
+		time.sleep(0.5)
 		GPIO.output(led2Pin,GPIO.HIGH)
 		time.sleep(1.5)
 		GPIO.output(led2Pin, GPIO.LOW) 
 		time.sleep(0.5)
-		GPIO.output(ledPin,GPIO.LOW)
-		time.sleep(0.5)
+		
 
 # End program cleanly with keyboard
 except KeyboardInterrupt:
