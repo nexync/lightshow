451d871c25b51a7c2008a88fa5fb03c9051d52c7
Jeffrey Cheng <imjeffrey23@gmail.com>
2020-12-15 15:41:04 -0500

    cleanup after gpio

--- main.py
+++ main.py
@@ -14,10 +14,13 @@ for i in pinlist:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, GPIO.HIGH)
 
-time.sleep(5)
+time.sleep(1)
 p.cascade(pinlist,0.2,0.1)
+time.sleep(1)
 p.cascade(pinlist,0.2,0.1)
+time.sleep(1)
 p.stairup(pinlist,0.1,1)
+time.sleep(1)
 p.blink(pinlist,0.5)
 
 GPIO.cleanup()
