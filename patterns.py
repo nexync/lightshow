ff1d54c315915295718cd56dc34af640b60b7146
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-18 16:02:21 -0500

    alternate

--- patterns.py
+++ patterns.py
@@ -96,3 +96,16 @@ def flashb3(pinlist, timeon, timeoff):
 
 
 
+def switchsides(pinlist,time):
+    GPIO.output(pinlist[3],GPIO.LOW)
+    GPIO.output(pinlist[4],GPIO.LOW)
+    GPIO.output(pinlist[0], GPIO.HIGH)
+    GPIO.output(pinlist[1], GPIO.HIGH)
+    GPIO.output(pinlist[2], GPIO.HIGH)
+    time.sleep(timeon)
+    GPIO.output(pinlist[0],GPIO.LOW)
+    GPIO.output(pinlist[1],GPIO.LOW)
+    GPIO.output(pinlist[2],GPIO.LOW)
+    GPIO.output(pinlist[3],GPIO.HIGH)
+    GPIO.output(pinlist[4],GPIO.HIGH)
+    time.sleep(timeoff)
