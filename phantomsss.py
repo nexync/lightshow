6b49a5fd7ab218c6f1269ec4a04d17d537fa52d1
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-18 15:58:42 -0500

    alternate

--- phantomsss.py
+++ phantomsss.py
@@ -31,8 +31,9 @@ try:
     for i in range(1):
         p.stairup(pinlist, 0.35, 0.1)
     GPIO.output(pinlist[0],GPIO.LOW)
+    p.blink(0.5)
     for i in range(8):
-        p.flashb3(pinlist, 0.15, 0.01)
+        p.flashb3(pinlist, 0.20, 0.01)
     GPIO.cleanup()
 
 
