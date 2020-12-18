665a58de6d5361974ccbd158ab2d20bff4869aa1
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-18 11:40:22 -0500

    alternate

--- jazzy.py
+++ jazzy.py
@@ -57,9 +57,9 @@ try:
     GPIO.output(pinlist[0],GPIO.HIGH)
     p.blink(pinlist,0.5)
     for i in range (3):
-        p.blink(pinlist, 0.35)
-    p.stairup(pinlist, 0.1, 0.5)
-    p.stairdown(pinlist, 0.1, 0.5)
+        p.blink(pinlist, 0.5)
+    p.stairup(pinlist, 0.2, 0.5)
+    p.stairdown(pinlist, 0.2, 0.5)
 
 
 
