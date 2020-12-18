cbd01033c08ed45e7de6d2b1ae5935f539fc0e49
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-18 15:12:56 -0500

    alternate

--- music.py
+++ music.py
@@ -4,3 +4,8 @@ def sleigh(starttime):
     pygame.mixer.init()
     pygame.mixer.music.load("sleigh.mp3")
     pygame.mixer.music.play(loops = 1, start = starttime)
+
+def phantom(starttime):
+    pygame.mixer.init()
+    pygame.mixer.music.load("phant.mp3")
+    pygame.mixer.music.play(loops = 1, start = starttime)
