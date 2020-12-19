28281e1208d92eff19a1fe7c0ed8f28aff1a7604
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-18 22:17:20 -0500

    alternate

--- jazzy.py
+++ jazzy.py
@@ -72,7 +72,7 @@ try:
     for i in range(2):
         p.blink(pinlist, 0.3)
         time.sleep(0.1)
-    for i in range(2):
+    for i in range(3):
         p.alternate(pinlist,0.3)
 
     # for i in range(10):
