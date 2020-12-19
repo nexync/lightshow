c2d4af6ae379da6da1688ed3988a950868d735dc
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-18 22:04:46 -0500

    alternate

--- jazzy.py
+++ jazzy.py
@@ -17,38 +17,38 @@ for i in pinlist:
 p.off(pinlist)  
 
 try:
-    m.sleigh(1)
+    m.sleigh(16)
     # first 15 seconds
-    for i in range(4):
-        p.alternate(pinlist, 0.635)
-    p.blink(pinlist, 0.75)
-    p.cascade(pinlist,0.25,0.1)
-    p.blink(pinlist, 0.75)
-    time.sleep(0.4)
-    for i in range(3):
-        p.blink(pinlist, 0.2)
-        time.sleep(0.10)
-    time.sleep(0.5)
-    p.stairup(pinlist,0.1,0.5)
-    time.sleep(0.5)
-    p.stairdown(pinlist,0.1,0.5)
-    time.sleep(0.32)
-    for i in range(3):
-        p.blink(pinlist, 0.2)
-        time.sleep(0.10)
+    # for i in range(4):
+    #     p.alternate(pinlist, 0.635)
+    # p.blink(pinlist, 0.75)
+    # p.cascade(pinlist,0.25,0.1)
+    # p.blink(pinlist, 0.75)
+    # time.sleep(0.4)
+    # for i in range(3):
+    #     p.blink(pinlist, 0.2)
+    #     time.sleep(0.10)
+    # time.sleep(0.5)
+    # p.stairup(pinlist,0.1,0.5)
+    # time.sleep(0.5)
+    # p.stairdown(pinlist,0.1,0.5)
+    # time.sleep(0.32)
+    # for i in range(3):
+    #     p.blink(pinlist, 0.2)
+    #     time.sleep(0.10)
+    # for i in range(2):
+    #     p.alternate(pinlist, 0.635)
+    # for i in range (2):
+    #     p.cascade(pinlist,0.2,0.1)
+    # p.closein(pinlist, 0.35, 0.1)
+    # p.alternate(pinlist, 0.35)
+    # p.closein(pinlist, 0.5, 0.1)
+    # time.sleep(0.35)
+    # for i in range (4):
+    #     p.blink(pinlist, 0.25)
+    #     time.sleep(0.1)
     for i in range(2):
-        p.alternate(pinlist, 0.635)
-    for i in range (2):
-        p.cascade(pinlist,0.2,0.1)
-    p.closein(pinlist, 0.35, 0.1)
-    p.alternate(pinlist, 0.35)
-    p.closein(pinlist, 0.5, 0.1)
-    time.sleep(0.35)
-    for i in range (4):
-        p.blink(pinlist, 0.25)
-        time.sleep(0.1)
-    for i in range(2):
-        p.alternate(pinlist, 0.5)
+        p.alternate(pinlist, 0.65)
     GPIO.output(pinlist[4], GPIO.HIGH)
     time.sleep(0.25)
     GPIO.output(pinlist[3], GPIO.HIGH)
