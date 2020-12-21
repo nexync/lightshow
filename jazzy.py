22d0e3726d3844d062abcf7e93ef0ebb96c7f233
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-20 23:32:10 -0500

    helo

--- jazzy.py
+++ jazzy.py
@@ -77,14 +77,14 @@ try:
     for i in range(4):
         p.alternate(pinlist,0.32)
     for i in range(4):
-        p.closein(pinlist, 0.25, 0.01)
+        p.closein(pinlist, 0.32, 0.01)
     for i in range(4):
         p.switchsides(pinlist, 0.33)
     for i in range(4):
         p.alternate(pinlist,0.33)
     for i in range(2):
         p.cascade(pinlist, 0.25, 0.1)
-    for i in range(1):
+    for i in range(2):
         p.closein(pinlist, 0.25, 0.1)
     time.sleep(0.05)
     for i in range(4):
