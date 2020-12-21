62bb5850fda51b84497ed1168766b03cf9e60a71
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-21 14:51:21 -0500

    helo

--- jazzy.py
+++ jazzy.py
@@ -104,7 +104,7 @@ try:
         p.buildin(pinlist, 0.31)
         p.collapsein(pinlist, 0.33)
         p.buildout(pinlist,0.31)
-    for i in range(2):
+    for i in range(3):
         p.collapsein(pinlist, 0.33)
         p.collapseout(pinlist, 0.33)
     for i in range(3):
