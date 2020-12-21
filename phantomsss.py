a011f5d9173f71002b9155cd316c7b721e83a708
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-20 19:04:59 -0500

    helo

--- phantomsss.py
+++ phantomsss.py
@@ -56,9 +56,10 @@ try:
     GPIO.output(pinlist[4],GPIO.LOW)
     for i in range(1):
         p.cascade(pinlist,0.25, 0.1)
+    p.cascade(pinlist, 0.25, 0.1)
     for i in range(4):
         p.alternate(pinlist, 0.23)
-    time.sleep(1)
+    time.sleep(0.35)
     for i in range(3):
         p.blink(pinlist, 0.2)
         time.sleep(0.05)
