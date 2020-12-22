23e7bc2433d03b52d837b43372a73444d20b13db
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-22 15:48:15 -0500

    helo

--- jazzy.py
+++ jazzy.py
@@ -96,6 +96,7 @@ try:
     # for i in range(3):
     #     p.switchsides(pinlist, 0.32)
     # # time.sleep(0.5)
+    time.sleep(0.3)
     for i in range(5):
         p.blink(pinlist, 0.3)
         time.sleep(0.1)
