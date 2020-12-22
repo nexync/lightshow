eda865437e87600d2156bae7d53b9d8807125dd3
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-22 16:08:43 -0500

    helo

--- phantomsss.py
+++ phantomsss.py
@@ -35,22 +35,22 @@ try:
     # for i in range(12):
     #     p.switchsides(pinlist, 0.2225)
     for i in range(4):
-        p.flashb3(pinlist, 0.2)
-        p.buildin(pinlist, 0.2)
-        time.sleep(0.2)
-        p.buildout(pinlist,0.2)
-        time.sleep(0.2)
+        p.flashb3(pinlist, 0.23)
+        p.buildin(pinlist, 0.23)
+        time.sleep(0.23)
+        p.buildout(pinlist,0.23)
+        time.sleep(0.23)
     for i in range(4):
-        p.collapsein(pinlist, 0.2)
-        time.sleep(0.2)
-        p.collapseout(pinlist, 0.2)
-        time.sleep(0.2)
+        p.collapsein(pinlist, 0.23)
+        time.sleep(0.23)
+        p.collapseout(pinlist, 0.23)
+        time.sleep(0.23)
     for i in range(2):
         p.flashb3(pinlist, 0.23)
         p.buildin(pinlist, 0.23)
         time.sleep(0.23)
         p.buildout(pinlist,0.23)
-        time.sleep(0.2)
+        time.sleep(0.23)
     for i in range(2):
         p.collapsein(pinlist, 0.225)
         time.sleep(0.225)
